8de0ef7782c48f61ff4e721e71ce774774c1ff8f
Israel Cavalcante <101607067+IsraelCavalcante1@users.noreply.github.com>
2023-01-18 20:51:55 -0300

    first commit

---
+++ src/main/java/br/com/fiap/cidadeorganica/cidade/CidadeOrganicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cidadeorganica.cidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeOrganicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeOrganicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/cidadeorganica/cidade/CidadeOrganicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cidadeorganica.cidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeOrganicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
